8f32ba2d2b62be74f6c8d55a0ed4381a6f4a2a08
Hunter <eugyneehunter@gmail.com>
2025-03-08 01:30:31 +0300

    CORS configuration

--- server/server.js
+++ server/server.js
@@ -3,21 +3,27 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js'
+import authRouter from './routes/authRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }))
+
+// Allow CORS from any origin with credentials
+app.use(cors({
+    origin: (origin, callback) => {
+        callback(null, true); // Allow any origin
+    },
+    credentials: true // Allow credentials
+}));
 
 app.get('/', (req, res) => res.send("API WORKING CORRECTLY"));
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
